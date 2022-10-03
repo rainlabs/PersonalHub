701699e39826fd8f78d293eecb15dcf835ddb5ce
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-03 23:17:44 +0300

    comment out not_found style path

--- client/src/components/pages/not_found.page.tsx
+++ client/src/components/pages/not_found.page.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import '../../styles/home.page.css'
+// import '../../styles/home.page.css'
 import { Link } from 'react-router-dom';
 
 const NotFoundPage: FC = () => {
