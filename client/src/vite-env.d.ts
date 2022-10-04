27fa937f47c8ca9b95964bafefee29212e4a0515
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-04 14:01:55 +0300

    update configs

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_BACKEND_URI: string
+    readonly VITE_BACKEND_URI: string,
+    readonly VITE_PORT: string
     // more env variables...
   }
 
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './build'
-  },
-  server: {
-    port: 3050
+export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: './build'
+    },
+    server: {
+      port: Number(process.env.VITE_PORT),
+      host: true
+    }
   }
 })
