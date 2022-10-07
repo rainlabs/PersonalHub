4330289a84163f36785e4334bd1f97cd31a16607
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-08 01:35:49 +0300

    removing unused service override

--- server/src/api/article/services/article.ts
+++ server/src/api/article/services/article.ts
@@ -3,14 +3,5 @@
  */
 
 import { factories } from '@strapi/strapi';
-import { CollectionTypeService, GenericService } from '@strapi/strapi/lib/core-api/service';
 
-type ListService = GenericService & {
-    find?(args: any): void;
-};
-
-export default factories.createCoreService<ListService>('api::article.article', ({ strapi }) => ({
-    async find(...args) {
-        return super.find(...args)
-    }
-} as ListService));
+export default factories.createCoreService('api::article.article');
