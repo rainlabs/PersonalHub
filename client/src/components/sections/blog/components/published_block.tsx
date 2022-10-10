f583735288e28761e52a4bd09af615c0da0afee7
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-10 03:24:27 +0300

    adding tags for instantview #3

--- client/src/components/sections/blog/components/published_block.tsx
+++ client/src/components/sections/blog/components/published_block.tsx
@@ -11,9 +11,9 @@ type Props = {
 const BlogPublishedBlock: FC<Props> = ({ label, id, className, children }) => {
     const defaultLabel = 'Опубликовано'
     return (
-        <p className={`text-gray-500 mb-6 ${className}`}>
+        <p className={`text-gray-500 mb-6 ${className || ''} ${id || ''}`}>
             <small>
-                { label || defaultLabel } <u><time id={id || ''} dateTime={new Date(children).toUTCString()}>{ children ? DateUtils.extractDate(children) : 'N/A' }</time></u>
+                { label || defaultLabel } <u><time id={id || ''} dateTime={new Date(children).toISOString()}>{ children ? DateUtils.extractDate(children) : 'N/A' }</time></u>
             </small>
         </p>
     )
