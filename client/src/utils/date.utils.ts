5c2c67b68cda4ddf07e87b09dd75167ffbee7ebd
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-11 10:47:18 +0300

    fixes date format

--- client/src/utils/date.utils.ts
+++ client/src/utils/date.utils.ts
@@ -1,14 +1,11 @@
 export default {
     extractDate(raw_date: string | Date) {
-        // 13.01.2022
         const format: Intl.DateTimeFormatOptions = {
             day: '2-digit',
             month: '2-digit',
             year: 'numeric'
         }
         let date = new Date(raw_date)
-        date = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-
         return date.toLocaleString('ru-RU', format)
     }
 }
