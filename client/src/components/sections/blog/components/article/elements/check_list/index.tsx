22ca9eaf7859d883a7cebedcb19fcd5598eef6cb
Vladimir Ziablitskii <vladimir.ziablitskii@sibirianit.ru>
2022-10-16 10:46:42 +0300

    checklist classes for instantview

--- client/src/components/sections/blog/components/article/elements/check_list/index.tsx
+++ client/src/components/sections/blog/components/article/elements/check_list/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const BlogCheckList: FC<Props> = ({ className, list }) => {
     return (
-        <div className={`border-2 border-slate-100 dark:border-slate-700 bg-slate-100 dark:bg-slate-700 rounded-lg my-4 mx-0 md:mx-32 lg:mx-48 space-y-0.5 ${className || ''}`}>
+        <div className={`border-2 border-slate-100 dark:border-slate-700 bg-slate-100 dark:bg-slate-700 rounded-lg my-4 mx-0 md:mx-32 lg:mx-48 space-y-0.5 checklist ${className || ''}`}>
             {
                 list.map((el, idx) => <BlogCheckListItem key={idx} {...el} />)
             }
--- client/src/components/sections/blog/components/article/elements/check_list/item.tsx
+++ client/src/components/sections/blog/components/article/elements/check_list/item.tsx
@@ -9,7 +9,7 @@ export type CheckListItemProps = {
 
 const BlogCheckListItem: FC<CheckListItemProps> = ({name, done}) => {
     return (
-        <div className="flex items-center justify-between text-gray-800 dark:text-gray-200 bg-slate-50 dark:bg-slate-800 first:rounded-t-lg last:rounded-b-lg px-4 py-2">
+        <div className={`flex items-center justify-between text-gray-800 dark:text-gray-200 bg-slate-50 dark:bg-slate-800 first:rounded-t-lg last:rounded-b-lg px-4 py-2  ${done ? 'item-success' : 'item-failure'}`}>
             <p>{ name }</p>
 
             <div className={`w-5 h-5 sm:h-7 sm:w-7 ${done ? 'text-green-500' : 'text-red-400'}`}>
